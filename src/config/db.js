a84d2610735adcef6fbf28add78efbbd55bfe2f6
yukti patle <yuktipatle563@gmail.com>
2026-06-26 16:21:00 +0530

    Configure TiDB Cloud SSL

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,19 @@
+const fs = require("fs");
+const path = require("path");
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        ca: fs.readFileSync(
+            path.join(__dirname, "../../certs/isrgrootx1.pem")
+        )
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
